4b1a0f17f86d8a26dc85cc2a4793ecb813033fe5
Kenneth Bingham <w@qrk.us>
2023-11-12 13:13:44 -0500

    add drum studio

---
+++ content/blob/drum-studio/lib/scripts/generated.js
@@ -0,0 +1,1 @@
+let nodes={paths:["drum-setup.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["Drum setup"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
